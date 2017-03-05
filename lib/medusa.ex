6d081f2f08f36585d2fe4aeb3dbafbff36b1a837
Abraham Kuri <kurenn@icalialabs.com>
2017-03-05 14:33:32 -0600

    transforms the outputimage to base64

--- lib/medusa.ex
+++ lib/medusa.ex
@@ -1,11 +1,18 @@
 defmodule Medusa do
+  import ImageBase64Handler
+
   def merge_images(image_paths) do
-    #["image.jpg", "image.jpg"]
     convert(image_paths)
+    File.read! "/images/base64Output"
   end
 
-  def convert(image_paths) do
+  defp transform_to_base64 do
+    imagetobase64("/images/output.jpg", "/images/base64Output")
+  end
+
+  defp convert(image_paths) do
     System.cmd "convert", Enum.concat(image_paths, conversion_arguments()), stderr_to_stdout: true
+    transform_to_base64()
   end
 
   defp conversion_arguments do
--- lib/mix/tasks/merge_images.ex
+++ lib/mix/tasks/merge_images.ex
@@ -4,7 +4,7 @@ defmodule Mix.Tasks.MergeImages do
 
   @shortdoc "Merges images into one"
   def run(_) do
-    Medusa.merge_images(["/images/599302.jpg", "/images/12688205_759693460831697_1898124915733146135_n.jpg"]) |> IO.inspect
+    Medusa.merge_images(["/images/599302.jpg", "/images/12688205_759693460831697_1898124915733146135_n.jpg"]) |> IO.puts
   end
 
 end
